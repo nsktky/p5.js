8ad0de699e2e7c9e9da6cc618228765d20d2c2c1
nsktky <nosaka.takuya11@gmail.com>
2022-02-15 18:18:29 +0900

    20220214

---
+++ 20220214.js
@@ -0,0 +1,20 @@
+draw=_=>{createCanvas(W=600,W)
+  background(a=200)
+  noStroke()
+  f=fill
+  r=rect
+
+  f(c=50,d=100,c)
+  r(d, e=250, h=300, d ,10)
+  f("#c6b083")
+  r(a, b=400, c, a, c)
+  r(a, b, a, c ,c)
+  r(350, e, c, a ,c)
+  f("#82915b")
+  r(a, 180, h, d ,10)
+  r(h, h, h, d ,10)
+
+  f(W,W,a)
+  quad(d, g=500, a,W, b, W, g, g);
+
+  }
