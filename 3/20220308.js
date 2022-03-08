1ea695a2f7544f774c278cd8c38c3b415f64a5be
nsktky <nosaka.takuya11@gmail.com>
2022-03-08 22:41:46 +0900

    20220308

--- 3/20220308.js
+++ 3/20220308.js
@@ -4,8 +4,6 @@ let tileCountY = 10;
 let tileWidth = 0;
 let tileHeight = 0;
 
-let colorStep = 15;
-
 let circleCount = 0;
 let endSize = 0;
 let endOffset = 0;
@@ -19,23 +17,22 @@ function setup() {
   angleMode(DEGREES);
 
   createCanvas(windowWidth, windowHeight);
-  tileWidth = width / tileCountX;
-  tileHeight = height / tileCountY;
+  tileWidth = width;
+  tileHeight = height;
   actRandomSeed = random(10000);
   noFill();
-  stroke(245, 225, 173);
   strokeWeight(2);
 }
 
 function draw() {
-  background(76, 61, 22, 1);
+  background(169, 153, 190);
   randomSeed(actRandomSeed);
 
   translate(tileWidth / 2, tileHeight / 2);
 
-  circleCount = (frameCount*0.5) / 40 + 1;
-  endSize = map(cos(frameCount*2), -1, 1, tileWidth*0.8, tileWidth*0.1);
-  endOffset = map(sin(frameCount), -1, 1, -1 * (tileWidth - endSize) / 2, (tileWidth - endSize) / 2);
+  circleCount = (frameCount) / 20 + 1;
+  endSize = map(cos(frameCount), -1, 1, tileWidth*0.7, tileWidth*0.2);
+  endOffset = map(sin(frameCount*0.5), -1, 1, -1 * (tileWidth - endSize) / 2, (tileWidth - endSize) / 2);
 
   for (let gridY = 0; gridY <= tileCountY; gridY++) {
     for (let gridX = 0; gridX <= tileCountX; gridX++) {
@@ -56,7 +53,10 @@ function draw() {
       for (let i = 0; i < circleCount; i++) {
         let diameter = map(i, 0, circleCount, tileWidth, endSize);
         let offset = map(i, 0, circleCount, 0, endOffset);
-        rect(offset, 0, diameter);
+        stroke(217, 204, 160);
+        circle(offset, 0, diameter);
+        stroke(199, 208, 227);
+        circle(offset/4, 0, diameter/4);
       }
       pop();
     }
