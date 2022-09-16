83153d122bceaa96e2cec21fc576beef73b6fac4
nsktky <nosaka.takuya11@gmail.com>
2022-09-16 22:29:27 +0900

    20220916

--- 9/20220915.js
+++ 9/20220915.js
@@ -4,30 +4,30 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   background(200);
   angleMode(DEGREES);
-  size = 100
+  size = 100;
   count = 0;
   seed = random(10000000000000);
   noStroke();
 }
 
 function draw() {
-  randomSeed(seed)
+  randomSeed(seed);
   background(20);
   orbitControl();
-  translate(-width*0.1,width*0.1,0)
+  translate(-width * 0.1, width * 0.1, 0);
   rotateX(-40);
   rotateY(120);
 
   ambientLight(100);
   pointLight(255, 255, 255, 0, -500, 200);
-  for(let z = 1; z < size*10; z+=size) {
-    for(let x = 1; x < size*10; x+=size) {
+  for (let z = 1; z < size * 10; z += size) {
+    for (let x = 1; x < size * 10; x += size) {
       push();
-      translate(x, 0, z)
+      translate(x, 0, z);
       specularMaterial(250, 190, 20);
-      box(size,size+count*noise(z*x),size)
+      box(size, size + count * noise(z * x), size);
       pop();
-      count += random(0.0001, 0.04)
+      count += random(0.0001, 0.04);
     }
   }
 }
