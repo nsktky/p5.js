da5d701bb2322cb9a93d5f693ac25ae2cb21b4fa
nsktky <nosaka.takuya11@gmail.com>
2022-09-17 13:33:51 +0900

    20220917

--- 9/20220916.js
+++ 9/20220916.js
@@ -3,18 +3,18 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   rectMode(CENTER);
-  count = 1;
+  count = 1
   noStroke();
 }
 
 function draw() {
-  background(0);
-  grid = 10;
-  for (let y = 0; y <= height; y += grid) {
-    for (let x = 0; x <= width; x += grid) {
-      let size = map(noise(x, y), 0, 1, 0, grid);
-      rect(x, y, size + count);
-      count = map(cos(frameCount * 0.005), -1, 1, -grid, grid);
+  background(0)
+  grid = 10
+  for(let y = 0; y <= height; y += grid){
+    for(let x = 0; x <= width; x += grid){
+      let size = map(noise(x, y), 0, 1, 0, grid)
+      rect(x,y,size+count)
+      count = map(cos(frameCount*0.005),-1, 1, -grid, grid)
     }
   }
 }
---
+++ 9/20220917.js
@@ -0,0 +1,23 @@
+let count;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(230);
+  rectMode(CENTER);
+  count = 1;
+  noStroke();
+}
+
+function draw() {
+  grid = 40;
+  for (let y = 0; y <= height; y += grid) {
+    for (let x = 0; x <= width; x += grid) {
+      let size = map(noise(x, y), 0, 1, 0, grid);
+      let r = map(noise(x), 0, 1, 150, 255);
+      let g = map(noise(x, y), 0, 1, 150, 255);
+      let b = map(noise(y), 0, 1, 150, 255);
+      fill(r, g, b);
+      circle(x+frameCount*0.1, y+count, size - count);
+      count = map(cos(frameCount * 0.005), -1, 1, 0, grid);
+    }
+  }
+}
