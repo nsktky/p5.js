a0025fd3ba0b78e684f77d759279e78acd937347
nsktky <nosaka.takuya11@gmail.com>
2022-09-20 23:52:44 +0900

    20220920

---
+++ 9/20220920.js
@@ -0,0 +1,27 @@
+let radius, count;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(60, 30, 60);
+  radius = 10;
+  count = 2;
+  rectMode(CENTER);
+  noStroke();
+}
+
+function draw() {
+  background(0);
+  for (let y = 0; y < height; y += radius) {
+    for (let x = 0; x < width; x += radius) {
+      let a = map(
+        noise(x * cos(frameCount * 0.0001), y * sin(frameCount * 0.0001)),
+        0,
+        1,
+        0,
+        255
+      );
+      fill(255, a);
+      circle(x, y, radius);
+    }
+  }
+}
