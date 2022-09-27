0ce42df1c724c9a66aa21bdc0fad8d73b8796013
nsktky <nosaka.takuya11@gmail.com>
2022-09-27 23:20:34 +0900

    20220928

---
+++ 9/20220927.js
@@ -0,0 +1,39 @@
+let noiseStep = 0.001;
+let timeStep = 0.005;
+let angleStep = 0.001;
+let angleStep2 = 600;
+let points = [];
+let grid = 30
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  background(0);
+  noStroke();
+  colorMode(HSB, 360, 100, 100, 100);
+
+  for(let y =0; y <= height+grid; y += grid){
+    for(let x =0; x <= width+grid; x += grid){
+      let p = createVector(x, y);
+      points.push(p);
+    }
+  }
+}
+
+function draw() {
+  for(let i = 0; i < points.length; i++){
+    let pointAngle = map(noise(points[i].x * angleStep, points[i].y * angleStep),0,1,0,angleStep2)
+    points[i].add(createVector(sin(pointAngle), cos(pointAngle)));
+    if (points[i].x > width) points[i].x = random(width);
+    if (points[i].x < 0) points[i].x = width;
+    if (points[i].y < 0) points[i].y = height;
+    if (points[i].y > height) points[i].y = random(height);
+
+    let colorAngle = map(
+      noise(points[i].x * noiseStep, points[i].y * noiseStep, frameCount * timeStep), 0, 1, 0, 360);
+    let h = map(sin(colorAngle), -1, 1, 10, 360);
+    let s = map(sin(colorAngle), -1, 1, 10, 720);
+    fill(h, s, 100, 100);
+    circle(points[i].x, points[i].y, grid)
+  }
+}
