c85079cafd628faf758982af2a4a81f0431b5640
nsktky <nosaka.takuya11@gmail.com>
2022-10-17 22:48:47 +0900

    20221017

---
+++ 10/20221017.js
@@ -0,0 +1,47 @@
+let noiseStep = 0.0001;
+let timeStep = 0.0001;
+let points = [];
+let grid = 500;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // angleMode(DEGREES);
+  colorMode(HSB, 360, 100, 100, 100);
+  background(0);
+  noStroke();
+
+  for (let y = 0; y <= height + grid; y += grid) {
+    for (let x = 0; x <= width + grid; x += grid) {
+      let p = createVector(x, y);
+      points.push(p);
+    }
+  }
+}
+
+function draw() {
+  beginShape();
+  for (let i = 0; i < points.length; i++) {
+    let angle = map(
+      noise(
+        points[i].x * noiseStep,
+        points[i].y * noiseStep,
+        frameCount * timeStep
+      ),
+      0,
+      1,
+      0,
+      360
+    );
+    let h = map(sin(angle), -1, 1, 10, 30);
+    let s = map(cos(angle), -1, 1, 10, 100);
+    let b = map(tan(angle), -1, 1, 10, 100);
+    let r = map(sin(angle), -1, 1, grid * 0.1, grid * 5);
+    fill(h, s, 100, 1);
+    stroke(h, 100, 100, 1);
+    ellipse(points[i].x, points[i].y, r, r);
+    fill(s, h, b, 10);
+    stroke(s, h, 100);
+    vertex(points[i].x, points[i].y);
+  }
+  endShape();
+}
