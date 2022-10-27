70375bd427d807f45450007d40927289468d8e3e
nsktky <nosaka.takuya11@gmail.com>
2022-10-28 00:30:35 +0900

    20221027

--- 10/20221027.js
+++ 10/20221027.js
@@ -4,9 +4,10 @@ let seed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(25);
+  background(250);
+  // angleMode(DEGREES)
 
-  grid = width / 10;
+  grid = width / 50;
   for (let x = 0; x < width + grid; x += grid) {
     for (let y = 0; y < height + grid; y += grid) {
       let p = createVector(x, y);
@@ -18,17 +19,13 @@ function setup() {
 }
 
 function draw() {
+  background(250, 5)
   randomSeed(seed)
   for (let i = 0; i < points.length; i++) {
     push();
     translate(points[i].x, points[i].y)
-    if (frameCount % 2 == 0) {
-      stroke(255);
-    } else if (frameCount % 3 == 0) {
-      stroke(0, 0, 255);
-    }  else {
-      stroke(255, 0, 0)
-    }
+    let c = map(noise(points[i].x, points[i].y, frameCount*0.01), 0, 1, 0, 360);
+    stroke(c)
     let step = random(10)
     line(0, 0, sin(frameCount*step) * grid * 0.5, cos(frameCount*step) * grid * 0.5)
     pop()
