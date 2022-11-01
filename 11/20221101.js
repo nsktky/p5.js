8676fb705dd2667495141565a3fe06d1bbc7bbb9
nsktky <nosaka.takuya11@gmail.com>
2022-11-01 22:52:29 +0900

    20221101

--- 11/20221101.js
+++ 11/20221101.js
@@ -4,12 +4,12 @@ let seed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(250);
+  background(200);
   // angleMode(DEGREES)
 
   colorMode(HSB, 360, 100, 100, 100)
 
-  grid = width / 3;
+  grid = width / 10;
   for (let x = 0; x < width + grid; x += grid) {
     for (let y = 0; y < height + grid; y += grid) {
       let p = createVector(x, y);
@@ -26,8 +26,8 @@ function draw() {
   for (let i = 0; i < points.length; i++) {
     push();
     let angle = map(noise(points[i].x * 0.001, points[i].y * 0.001),0,1,0,600)
-    let h = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount*0.01),0,1,0,20)
-    let s = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount*0.1),0,1,75,100)
+    let h = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount*0.01),0,1,0,360)
+    let s = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount*0.1),0,1,0,100)
     let b = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount*0.03),0,1,0,100)
 
     points[i].add(createVector(cos(angle), sin(angle)))
@@ -40,8 +40,6 @@ function draw() {
     stroke(h, s, b, 100)
     let step = random(0.001, 0.01)
     line(0, 0, sin(frameCount*step) * grid * 0.3 , cos(frameCount*step) * grid * 0.3)
-    stroke(h, b, s, 100)
-    line(0, 0, width / 2, height / 2)
     pop()
   }
 
