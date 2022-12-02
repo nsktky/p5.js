eb13812a8f52c89ca0ce41200ca902bfbb1d2462
nsktky <nosaka.takuya11@gmail.com>
2022-12-03 00:11:27 +0900

    20221202

--- 12/20221202.js
+++ 12/20221202.js
@@ -3,28 +3,28 @@ let x, y, newX, newY;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-  background(255);
-  stroke(0);
+  background(25);
+  stroke(250);
   noFill();
   angleMode(DEGREES);
   radius = min(width, height) / 2;
 }
 
 function draw(){
-  background(255);
+  background(25);
   translate(width/2, height/2);
 
   for(let j = 1; j < 30; j++) {
     beginShape();
     for(let i = 0; i < 360; i++){
-      let xoff = map(cos(i), -1, 1, 0, 10)
-      let yoff = map(sin(i), -1, 1, 0, 10)
+      let xoff = map(cos(i), -1, 1, 0, 3)
+      let yoff = map(sin(i), -1, 1, 0, 3)
       let r = map(noise(xoff, yoff, frameCount*0.0005), 0, 1, -radius + j, radius + j)
-      x = r * cos(r)
-      y = r * sin(r)
-      let a = 1
-      let b = 1
-      newX = sin(a * y)
+      x = radius * cos(r)
+      y = radius * sin(r)
+      let a = 0.1
+      let b = 0.1
+      newX = sin(a * x)
       newY = cos(b * y)
       vertex(newX * radius, newY * radius)
     }
