2b7c05bfdbafbc526a5ce7ce050757cd3fc02d1b
nsktky <nosaka.takuya11@gmail.com>
2022-12-08 22:06:12 +0900

    20221208-2

--- 12/20221208.js
+++ 12/20221208.js
@@ -3,8 +3,8 @@ let x, y, radius;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
   background(0);
-  stroke(255, 16, 20)
-  // noFill()
+  stroke(255)
+  noFill()
   angleMode(DEGREES)
   colorMode(HSB, 360, 100, 100)
   radius = width * 0.6
@@ -12,13 +12,13 @@ function setup() {
 
 function draw() {
   translate(width/2, height/2);
-  rotate(random(360))
+  rotate(random(100))
 
   beginShape()
   for(let i = 0; i < 360; i++) {
-    fill(i)
-    let xoff = map(sin(i), -1, 1, 0, 20)
-    let yoff = map(cos(i), -1, 1, 0, 20)
+    // fill(i)
+    let xoff = map(sin(i), -1, 1, 0, 10)
+    let yoff = map(cos(i), -1, 1, 0, 10)
     let angle = map(noise(xoff, yoff, frameCount*0.1), 0, 1, 0, 360)
     x = radius * sin(angle) / frameCount * 50
     y = radius * cos(angle) / frameCount * 50
