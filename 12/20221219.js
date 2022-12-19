19495f1fc476ce596747e7f293754dac30135e89
nsktky <nosaka.takuya11@gmail.com>
2022-12-19 23:37:20 +0900

    20221219

--- 12/20221219.js
+++ 12/20221219.js
@@ -5,23 +5,24 @@ function setup() {
   background(200)
   // angleMode(DEGREES)
   radius = width * 0.3
+  strokeWeight(1)
   noFill()
 }
 
 function draw() {
-  background(200, 10)
+  // background(20, 10)
   translate(width/2, height/2);
   beginShape()
   for(let i = 0; i < 360; i++) {
-    fill(20, 100)
     // circle(random(-width,width), random(-height,height), random(10));
     let xoff = map(sin(i), -1, 1, 0, 0.1)
     let yoff = map(cos(i), -1, 1, 0, 0.1)
-    let angle = map(noise(xoff, yoff, frameCount*0.0001), 0,1,0,360)
+    let angle = map(noise(xoff, yoff, frameCount*0.001), 0,1,0,360)
 
-    x = radius * (sin(frameCount*0.01) / tan(angle))
-    y = radius * (cos(angle) / sin(i))
-    stroke(250,0,0)
+    y = radius * 0.3 * (sin(frameCount*0.01) - sin(angle) - sin(i))
+    x = radius * 0.3 * (cos(angle) + sin(i) / cos(frameCount*0.01))
+    stroke(angle,xoff*1000,yoff*1000)
+    fill(200, 50)
     vertex(x, y)
     
   }
