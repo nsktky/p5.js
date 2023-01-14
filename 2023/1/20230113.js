e12f710c971db0993b4a9b5cb4c48b28d5853535
nsktky <nosaka.takuya11@gmail.com>
2023-01-14 16:29:46 +0900

    20230113

---
+++ 2023/1/20230113.js
@@ -0,0 +1,28 @@
+let x, y, radius;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+  background(0);
+  angleMode(DEGREES);
+  radius = min(width, height) * 0.003
+  stroke(250, 190, 20)
+  blendMode(DIFFERENCE);
+}
+
+function draw() {
+  background(0)
+  translate(width/2, height/2);
+  for(let i = 0; i < 100; i++) {
+    let xoff = map(cos(i), -1, 1, 0, 360)
+    let yoff = map(sin(i), -1, 1, 0, 360)
+    let angle = map(noise(xoff, yoff, frameCount*0.001), 0, 1, 0, 360)
+    x = radius * i * cos(xoff - frameCount)
+    y = radius * i * sin(yoff + frameCount) * cos(angle)
+    fill(xoff, i, yoff)
+    circle(x, y, radius*50)
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
