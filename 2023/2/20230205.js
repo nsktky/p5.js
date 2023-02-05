9b6319b4f5d7bd61a9b202072a5c95f30fb0dde7
nsktky <nosaka.takuya11@gmail.com>
2023-02-05 19:03:31 +0900

    20230205

---
+++ 2023/2/20230205.js
@@ -0,0 +1,39 @@
+let x, y, radius;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  colorMode(HSB, 360, 100, 100, 100)
+  radius = min(width, height) * 0.3;
+  x = 0;
+  y = 0;
+  blendMode(DIFFERENCE);
+  noStroke();
+}
+
+function draw() {
+  push()
+  translate(width/2, height/2);
+  rotate(frameCount)
+  drawImage(width, height / 2, -1)
+  drawImage(0, height / 2, 1)
+  pop()
+}
+
+function drawImage(ox, oy, s) {
+  push();
+  translate(ox, oy);
+  for(let i = 0; i < 300; i++){
+    let angle = map(noise(x, y, frameCount), 0, 1, 0, 360);
+    let xoff = map(sin(angle), -1, 1, 0, 360);
+    let yoff = map(cos(angle), -1, 1, 0, 360);
+    x = radius * sin(xoff);
+    y = radius * tan(xoff);
+    fill(angle, x, angle/yoff/xoff);
+    let move = map(sin(frameCount*0.2), -1, 1, 0, width) * s
+    ellipse(x + move, y, angle*0.3)
+  }
+  pop();
+}
