41eba5d68c0280206eac09e48688eeaaeaea9149
nsktky <nosaka.takuya11@gmail.com>
2023-03-20 22:46:12 +0900

    20230320

---
+++ 2023/3/20230320.js
@@ -0,0 +1,48 @@
+let particles = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255)
+  for (let i = 0; i < 20; i++) {
+    particles.push(new Particle());
+  }
+  blendMode(DIFFERENCE)
+}
+
+function draw() {
+  // background(255, 10);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+  }
+}
+
+class Particle {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.mass = random(1, 3);
+  }
+
+  update() {
+    let gravity = createVector(width/2, height/2);
+    gravity.sub(this.position);
+    let distance = gravity.mag();
+    distance = constrain(distance, 5, 25);
+    let force = 1 * this.mass / (distance * distance);
+    gravity.normalize();
+    gravity.mult(force);
+    this.acceleration = gravity;
+
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(5);
+    this.position.add(this.velocity);
+  }
+
+  show() {
+    noStroke();
+    fill(150, map(noise(this.velocity.x, this.velocity.y, frameCount*0.01), 0, 1, 0, 255), 100);
+    circle(this.position.x, this.position.y, width*0.1);
+  }
+}
