192c96812ac3627a82e0238b0aa9a19708d4a487
nsktky <nosaka.takuya11@gmail.com>
2023-03-29 20:45:21 +0900

    20230329

--- 2023/3/20230329.js
+++ 2023/3/20230329.js
@@ -14,8 +14,8 @@ function setup() {
     for (let y = 0; y < height + grid; y += grid) {
       let ox = random(width);
       let oy = random(height);
-      let r = map(noise(x * 0.001, y * 0.001, tan(ox) / sin(oy)), 0, 1, 180, 230);
-      let col = color(r, r * 0.5, 50, 100);
+      let r = map(noise(x * 0.001, y * 0.001, tan(ox) / sin(oy)), 0, 1, 180, 360);
+      let col = color(r, 100, 100, 100);
       let p = createVector(ox, oy, col);
       points.push(p);
     }
@@ -26,19 +26,19 @@ function setup() {
 function draw() {
   for (let i = 0; i < points.length; i++) {
     push();
-    let angle = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount * 0.0005), 0, 1, 0, 360);
-    points[i].x += sin(angle) * 0.5;
-    points[i].y += cos(angle) * 0.5;
+    let angle = map(noise(points[i].x * 0.01, points[i].y * 0.01, frameCount * 0.002), 0, 1, 0, 360);
+    points[i].x += sin(angle) * 2;
+    points[i].y += cos(angle) * 2;
     if (points[i].x < 0) points[i].x = width;
     if (points[i].x > width) points[i].x = 0;
     if (points[i].y < 0) points[i].y = height;
     if (points[i].y > height) points[i].y = 0;
 
     translate(points[i].x, points[i].y);
-    rotate(i * frameCount * 0.0005);
+    rotate(i * frameCount * 0.002);
     fill(points[i].z);
     ellipse(0, 0, counter);
     pop();
   }
-  counter = sin(frameCount * 0.025) * grid * 0.3;
+  counter = sin(frameCount * 0.1) * grid * 0.5;
 }
