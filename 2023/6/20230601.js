447168731ead8aba8401a2d77535589748c5903a
nsktky <nosaka.takuya11@gmail.com>
2023-06-01 23:01:21 +0900

    20230601

---
+++ 2023/6/20230601.js
@@ -0,0 +1,41 @@
+let points = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
+  background(20);
+  strokeWeight(1)
+  for (let i = 0; i < 50; i++) {
+    let x = random(width); 
+    let y = random(height);
+    let angle = random(360);
+    let col = color(random(170, 200), random(120, 200), 200); 
+    points.push({x: x, y: y, angle: angle, color: col}); 
+  }
+  // blendMode(DIFFERENCE)
+}
+
+function draw() {
+  // background(200, 180, 160);
+  for (let point of points) {
+    let x = point.x;
+    let y = point.y;
+    let angle = point.angle;
+    let col = point.color;
+    stroke(col);
+    for(let j = 0; j < 100; j++){
+      let len = 5*sin(frameCount*0.1);
+      let endX = x + len * cos(angle);
+      let endY = y + len * sin(angle);
+      line(x, y, endX, endY);
+      x = endX;
+      y = endY;
+      angle += random(20);
+    }
+    fill(0)
+    stroke(0)
+    circle(point.x + 10, point.y, 5)
+    circle(point.x - 10, point.y, 5)
+    ellipse(point.x, point.y + 10, 10, 1)
+  }
+}
