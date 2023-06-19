da0dad6fd1500e335a20b1b89726f2ae71a8ae3f
nsktky <nosaka.takuya11@gmail.com>
2023-06-19 22:03:29 +0900

    20230619

--- 2023/6/20230619.js
+++ 2023/6/20230619.js
@@ -1,7 +1,7 @@
 let points = [];
 let img;
 function preload() {
-  img = loadImage("1.png")
+  img = loadImage("3.png")
 }
 
 function setup() {
@@ -14,15 +14,15 @@ function setup() {
   img.loadPixels();
   image(img, 0, 0);
 
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 20; i++) {
     let x = random(width); 
     let y = random(height); 
-    let angle = random(360);
+    let angle = random(36);
     let pixColor = color(img.get(x, y));
-    pixColor.setAlpha(40);
+    pixColor.setAlpha(5);
     points.push({x: x, y: y, angle: angle, color: pixColor}); 
   }
-  blendMode(BURN)
+  // blendMode(BURN)
 }
 
 function draw() {
@@ -32,11 +32,12 @@ function draw() {
     let angle = point.angle;
     let col = point.color;
     stroke(col);
-    for(let i = 0; i < 10; i++){
+    noFill();
+    for(let i = 0; i < 30; i++){
       let len = map(sin(frameCount*0.1), -1, 1, 0, 40);
       let endX = x + len * cos(angle);
       let endY = y + len * sin(angle);
-      line(x, y, endX, endY);
+      rect(x, y, endX, endY);
       x = endX;
       y = endY;
       angle += random(360);
@@ -46,6 +47,6 @@ function draw() {
 
 function keyPressed() {
   if (key == 's'){
-    saveCanvas('20230618', 'png');
+    saveCanvas('20230619', 'png');
   }
 }
