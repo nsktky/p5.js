a325e112ce7acf12b20febd4450fb0c512b22eae
nsktky <nosaka.takuya11@gmail.com>
2023-06-28 21:33:30 +0900

    20230628

---
+++ 2023/6/20230628.js
@@ -0,0 +1,82 @@
+class Lightning {
+  constructor() {
+    this.path = [];
+    this.isAlive = true;
+    this.maxSteps = random(10, height*0.4); // 雷の最大の長さ
+    this.noiseScale = 100; // パーリンノイズのスケール
+    this.reset();
+    // frameRate(20)
+    blendMode(DIFFERENCE)
+    colorMode(HSB, 360, 100, 230, 20)
+  }
+
+  reset() {
+    this.path = [];
+    let start = createVector(random(width), 0);
+    this.path.push(start);
+    this.noiseOffset = random(100);
+    this.currentStep = 0;
+    this.opacity = 255;
+    this.speed = random(5, 10); // 追加: 雷のスピード
+    this.isAlive = !this.isAlive
+  }
+
+  generateStep() {
+    let lastPoint = this.path[this.path.length - 1];
+    let newX = lastPoint.x + map(noise(this.noiseOffset), 0, 1, -25, 25);
+    let newY = lastPoint.y + map(noise(this.speed), 0, 1, 0, 15); // スピードに応じてY座標を更新
+    let newPoint = createVector(newX, newY);
+    this.path.push(newPoint);
+    this.noiseOffset += this.noiseScale;
+    this.currentStep++;
+    this.opacity -= (350 / this.maxSteps);
+    if (this.currentStep >= this.maxSteps) {
+      this.isAlive = false;
+    }
+  }
+
+  update() {
+    if (this.isAlive) {
+      this.generateStep();
+    } else {
+      this.reset();
+    }
+  }
+
+  draw() {
+    stroke(this.currentStep, this.noiseOffset, this.opacity, this.opacity);
+    noFill();
+    beginShape();
+    for (let point of this.path) {
+      vertex(-point.x, point.y);
+      vertex(point.x, point.y);
+      // vertex(point.x, point.y);
+      // vertex(point.x+10, point.y+10);
+    }
+    endShape(CLOSE);
+  }
+}
+
+let lightningBolts = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 20; i++) {
+    lightningBolts[i] = new Lightning();
+  }
+  strokeWeight(10)
+}
+
+function draw() {
+  background(0);
+  for (let bolt of lightningBolts) {
+    bolt.update();
+    bolt.draw();
+  }
+}
+
+function keyPressed() {
+  if (key == 's'){
+    saveCanvas('20230628-2', 'png');
+  }
+}
