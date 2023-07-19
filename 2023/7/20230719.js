42a457095499558d29ac0a3149ce02f1568f616c
nsktky <nosaka.takuya11@gmail.com>
2023-07-19 21:49:32 +0900

    20230719

---
+++ 2023/7/20230719.js
@@ -0,0 +1,43 @@
+let grid;
+let points = [];
+let count = 0
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  angleMode(DEGREES);
+  noStroke();
+  // rectMode(CENTER);
+  grid = width / 60;
+  for(let x = 0; x <= width + grid; x += grid) {
+    let c;
+    let y = height / 2;
+    if(count % 2 == 0){
+      c = 255
+      fill(c);
+    } else {
+      c = 0
+      fill(c);
+    }
+    let p = createVector(x, y, c)
+    points.push(p);
+    rect(x, y, grid);
+    count += 1
+  }
+}
+
+function draw() {
+  for(let i = 0; i < points.length; i++) {
+    let angle = map(sin(frameCount), -1, 1, 0, 360)
+    let p = createVector(cos(angle)*0.5, sin(angle)*3);
+    points[i].add(p)
+    fill(color(points[i].z))
+    rect(points[i].x, points[i].y, grid*1.1);
+  }
+}
+
+function keyPressed() {
+  if (key == 's'){
+    saveCanvas('20230719-2', 'png');
+  }
+}
