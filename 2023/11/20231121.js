953579e85b3842a21884d7fbf92f0cedd26f914e
nsktky <nosaka.takuya11@gmail.com>
2023-11-21 21:07:26 +0900

    20231121

--- 2023/11/20231121.js
+++ 2023/11/20231121.js
@@ -11,17 +11,17 @@ function setup() {
 }
 
 function draw() {
-  background(0, 10)
+  background(0, 1)
   for(let ox = 0; ox < width + grid; ox += grid){
     for(let oy = 0; oy < height + grid; oy += grid){
       beginShape()
       for(let i = 0; i < 36; i++) {
         stroke(random(200, 250), 100, 20);
-        let angle = speed * frameCount - random(10);
+        let angle = speed * frameCount - random(20);
         let r = grid;
-        let x = r * cos(angle+i)*sin(angle);
+        let x = r * cos(angle)/sin(angle);
         let y = r * cos(angle);
-        vertex(ox + x, oy + y)
+        vertex(ox + y, oy + x)
       }
       endShape(CLOSE);
     }
@@ -30,6 +30,6 @@ function draw() {
 
 function keyPressed() {
   if (key == 's'){
-    saveCanvas('20231120-2', 'png');
+    saveCanvas('20231121-2', 'png');
   }
 }
