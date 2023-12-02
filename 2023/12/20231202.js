45317aa1beb3a4f6ae7044afb9f3eb76a8446af2
nsktky <nosaka.takuya11@gmail.com>
2023-12-02 21:41:07 +0900

    20231202

---
+++ 2023/12/20231202.js
@@ -0,0 +1,43 @@
+let grid;
+let points = [];
+let mult = 0.01;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(22, 20, 30);
+  noStroke()
+  // angleMode(DEGREES);
+  grid = min(width, height) / 30
+
+  for(let x = 0; x < width + grid; x+=grid) {
+    for(let y = 0; y < height + grid; y+=grid) {
+      let z = random()
+      let p = createVector(random(width), random(height), z)
+      points.push(p);
+    }
+  }
+}
+
+function draw() {
+  // background(22, 20, 30)
+  for(let i = 0; i < points.length; i++) {
+    let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 100, 300);
+    let x = sin(angle);
+    let y = cos(angle);
+    points[i].add(createVector(x, y));
+
+    if(points[i].x > width) points[i].x = random(width);
+    if(points[i].x < 0) points[i].x = random(width);
+    if(points[i].y > height) points[i].y = random(height);
+    if(points[i].y > height) points[i].y = random(height);
+
+    fill(angle, 190, 20);
+    circle(points[i].x, points[i].y, 1);
+  }
+}
+
+function keyPressed() {
+  if (key == 's'){
+    saveCanvas('20231202-2', 'png');
+  }
+}
