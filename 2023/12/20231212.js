2292e7ea87e915e0258fab0efaffe589cd09fa44
nsktky <nosaka.takuya11@gmail.com>
2023-12-12 21:17:25 +0900

    20231212

---
+++ 2023/12/20231212.js
@@ -0,0 +1,44 @@
+let grid;
+let points = [];
+let mult = 0.1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(22, 20, 30);
+  noStroke();
+  colorMode(HSB, 360, 100, 100, 100)
+  // angleMode(DEGREES);
+  grid = min(width, height) / 100
+
+  for(let x = 0; x < width + grid; x+=grid) {
+    for(let y = 0; y < height + grid; y+=grid) {
+      let z = random()
+      let p = createVector(random(width), random(height), z)
+      points.push(p);
+    }
+  }
+}
+
+function draw() {
+  // background(0, 2)
+  for(let i = 0; i < points.length; i++) {
+    let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 10);
+    let x = sin(angle);
+    let y = tan(i)*cos(angle);
+    points[i].add(createVector(x, y));
+
+    if(points[i].x > width) points[i].x = random(width);
+    if(points[i].x < 0) points[i].x = random(width);
+    if(points[i].y > height) points[i].y = random(height);
+    if(points[i].y > height) points[i].y = random(height);
+
+    fill(angle*0.2, 30, 100);
+    circle(points[i].x, points[i].y, 1);
+  }
+}
+
+function keyPressed() {
+  if (key == 's'){
+    saveCanvas('20231212-2', 'png');
+  }
+}
