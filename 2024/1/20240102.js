d82b7a4282ba36e1e43c7975ead0eeb9ddbb2b6d
nsktky <nosaka.takuya11@gmail.com>
2024-01-02 19:44:08 +0900

    20240102

---
+++ 2024/1/20240102.js
@@ -0,0 +1,54 @@
+let grid;
+let points = [];
+let mult = 0.001;
+
+let colorList = [];
+for(let i = 0; i < 100; i++) {
+  let color = (Math.random() * 0xFFFFFF | 0).toString(16);
+  let randomColor = "#" + ("000000" + color).slice(-6);
+  colorList[i] = randomColor;
+}
+
+let outputColors = [
+  chroma.scale(colorList).mode("rgb").out("hex"),
+];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+  // angleMode(DEGREES);
+  grid = min(width, height) / 80
+
+  for(let x = 0; x < width + grid; x+=grid) {
+    for(let y = 0; y < height + grid; y+=grid) {
+      let z = random()
+      let p = createVector(random(width), random(height), z)
+      points.push(p);
+    }
+  }
+}
+
+function draw() {
+  // background(0, 10)
+  for(let i = 0; i < points.length; i++) {
+    let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 10);
+    let x = sin(i)/cos(angle);
+    let y = sin(angle);
+    points[i].add(createVector(x, y));
+
+    if(points[i].x > width) points[i].x = random(width);
+    if(points[i].x < 0) points[i].x = random(width);
+    if(points[i].y > height) points[i].y = random(height);
+    if(points[i].y > height) points[i].y = random(height);
+
+    fill(outputColors[0](map(y, -1, 1, 0, 1)));
+    circle(points[i].x, points[i].y, 1);
+  }
+}
+
+function keyPressed() {
+  if (key == 'j'){
+    saveCanvas('20240102-2', 'png');
+  }
+}
